8390d32e23d4c34fa80858b64e1801642d925cd1
quan-p <m.phamquan@gmail.com>
2022-09-29 18:37:44 -0500

    Add test avatar placeholders

--- models/userModel.js
+++ models/userModel.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: string, required: true, maxlength: 25 },
+    username: { type: String, required: true, maxlength: 25 },
     password: { type: String, required: true, minLength: 1, maxLength: 50 },
     member: { type: Boolean, default: false },
     admin: { type: Boolean, default: false },
-    avatar: { type: String , required: true, enum: ["", "", "", "", "", ""], default: "" }
+    avatar: { type: String , required: true, enum: ["Test0", "Test1", "Test2", "Test3"], default: "Test0" }
 })
 
 module.exports = mongoose.model("User", UserSchema);
