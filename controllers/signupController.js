bd4e43c1fd7d67ead87c34ba654bd370d6856e40
quan-p <m.phamquan@gmail.com>
2022-10-04 15:28:29 -0500

    Add express validator

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const { body, validationResult } = require('express-validator');
 
 exports.signup_get = (req, res, next) => {
     res.render('signup-form', { title: "Sign Up Form"});
