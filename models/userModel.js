8587c245b60561c2f4db1eafe53efbdb392ee35a
quan-p <m.phamquan@gmail.com>
2022-10-05 16:22:24 -0500

    Increase max length of password to hold hashed length

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     username: { type: String, required: true, maxlength: 25 },
-    password: { type: String, required: true, minLength: 1, maxLength: 50 },
+    password: { type: String, required: true, minLength: 1, maxLength: 128 },
     member: { type: Boolean, default: false },
     admin: { type: Boolean, default: false },
     avatar: { type: String , required: true, enum: ["Test0", "Test1", "Test2", "Test3"], default: "Test0" }
