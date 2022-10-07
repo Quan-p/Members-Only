3616490a41083d2828aaa28f7bc174a518606c6b
quan-p <m.phamquan@gmail.com>
2022-10-07 17:30:38 -0500

    Add temporary redirect when user clicks login while already logged in

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 
 exports.login_get = (req, res, next) => {
+    if(res.locals.currentUser) return res.redirect('/');
     res.render('login_form', { title: "Login Form"});
 };
 exports.login_post = (req, res, next) => {
