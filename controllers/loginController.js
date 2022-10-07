3cdd77e8c2fff1e504c01c3b529ad317fed690af
quan-p <m.phamquan@gmail.com>
2022-10-07 17:33:19 -0500

    Add route with passport authentification to handle login request

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,6 +4,11 @@ exports.login_get = (req, res, next) => {
     if(res.locals.currentUser) return res.redirect('/');
     res.render('login_form', { title: "Login Form"});
 };
-exports.login_post = (req, res, next) => {
-    res.render('login_form', { title: "Login Form"});
-};
+
+exports.login_post = (
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/login"
+    })
+);
