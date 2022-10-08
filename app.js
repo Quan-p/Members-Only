147d1696948734f8f398e69418167a18ace18a0c
quan-p <m.phamquan@gmail.com>
2022-10-07 19:24:53 -0500

    Set up LocalStrategy to authenticate login

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ var path = require('path');
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const User = require('./models/userModel');
+const bcrypt = require("bcryptjs");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 
 var indexRouter = require('./routes/routes');
-var usersRouter = require('./routes/users');
  
 var app = express();
 
 // Set up mongoose connection
-const mongoose = require("mongoose");
+
 const mongoDB = process.env.MONGODB_URL;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
@@ -31,13 +33,50 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            // passwords match! log user in
+            return done(null, user)
+          } else {
+            // passwords do not match!
+            return done(null, false, { message: "Incorrect password" })
+          }
+      })
+      //return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
