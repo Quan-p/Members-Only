3e41d9274fc0064589c3f887760e4f5db48ce4cb
quan-p <m.phamquan@gmail.com>
2022-10-07 19:26:04 -0500

    Add in logout function

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,10 +5,17 @@ exports.login_get = (req, res, next) => {
     res.render('login_form', { title: "Login Form"});
 };
 
-exports.login_post = (
-    "/log-in",
-    passport.authenticate("local", {
+exports.login_post = (passport.authenticate("local", {
       successRedirect: "/",
       failureRedirect: "/login"
     })
 );
+
+exports.logout_get = (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/");
+    });
+}
