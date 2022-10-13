2c18acb2ac8d98dc64b7d0623785dd8ffcffe58e
quan-p <m.phamquan@gmail.com>
2022-10-13 16:45:52 -0500

    Use save method to update user member status

--- controllers/privilegeController.js
+++ controllers/privilegeController.js
@@ -1,13 +1,32 @@
 const User = require('../models/userModel');
+const { body, validationResult } = require('express-validator');
 
 // Member
 exports.member_get = (req, res, next) => {
     res.render('member_form', { title: 'Member Form', user: res.locals.currentUser});
 }
 
-exports.member_post = (req, res, next) => {
-    res.render('member_form');
-}
+exports.member_post = [
+    body('memberCode').trim().isLength({ min: 1 }).escape().withMessage('Password must be specified'),
+
+    async(req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.render('member_form', { title: 'Member Form', user: res.locals.currentUser, errors: errors.array() });
+        } else if (req.body.memberCode != process.env.MEMBER_PASSCODE) {
+            return res.render('member_form', { title: 'Member Form', user: res.locals.currentUser, passwordError: 'Incorrect Password' });
+        }
+
+        const updateUser = await User.findOne(res.locals.currentUser);
+        updateUser.member = true;
+
+        await updateUser.save(err => {
+            if(err) return next(err);
+            console.log(updateUser);
+            res.redirect('/member');
+        });
+    }
+]
  
 // Admin 
 exports.admin_get = (req, res, next) => {
