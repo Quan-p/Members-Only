6e18092dc0b8984a2fe26fee09152709e2ac015a
quan-p <m.phamquan@gmail.com>
2022-10-14 17:24:51 -0500

    Fix conditional to only render username and date if user is logged in AND they are a member

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ const Message = require('../models/messageModel');
 exports.index = async(req, res, next) => {
     try {
         const messages = await Message.find().sort([['timestamp', 'descending']]).populate('user');
-        return res.render('index', { title: 'Homepage', user: req.user, messages: messages })
+        return res.render('index', { title: 'Homepage', user: res.locals.currentUser, messages: messages })
     }
     catch (err) {
         return next(err);
