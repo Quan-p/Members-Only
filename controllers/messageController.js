c99461642e97ef661850a9a8836f25f1f4bea205
quan-p <m.phamquan@gmail.com>
2022-10-14 15:04:56 -0500

    Create POST request in message controller to create new messages

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,31 @@
 const Message = require('../models/messageModel');
+const { body, validationResult } = require('express-validator');
 
 exports.create_message_get = (req, res, next) => {
-    res.render('message_form', { title: 'Create a Message' });
+    res.render('message_form', { title: 'Write a Message', user: res.locals.currentUser });
 }
 
-exports.create_message_post = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Create Message POST");
-}
+exports.create_message_post = [
+    body('messageTitle').trim().isLength({ min: 1 }),
+    body('textInput').trim().isLength({ min: 1 }),
+
+    async(req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.render('message_form', { title: 'Write a Message', user: res.locals.currentUser, errors: errors.array() });
+        }
+
+        const message = new Message({
+            user: req.user.id,
+            title: req.body.messageTitle,
+            text: req.body.textInput,
+            timestamp: Date.now()
+        })
+
+        await message.save((err) => {
+            if(err) return next(err);
+            
+            res.redirect('/');
+        })
+    }
+];
