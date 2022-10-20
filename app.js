d0c83ed7032bc9aeb24d5499dfcd8a44e628d2ac
quan-p <m.phamquan@gmail.com>
2022-10-20 18:51:54 -0500

    Change mongodb uri

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 
 // Set up mongoose connection
 
-const mongoDB = process.env.MONGODB_URL;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
