d69b2092b8128af978b90692775568deb6d011af
quan-p <m.phamquan@gmail.com>
2022-10-20 19:11:38 -0500

    Move dotenv to top

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,7 +10,6 @@ const User = require('./models/userModel');
 const bcrypt = require("bcryptjs");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config();
 
 var indexRouter = require('./routes/routes');
  
